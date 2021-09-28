03d47907a92fe8efd9818288e803a1cb149f4df5
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-09-28 06:21:25 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,12 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <h6 class="text-center text-warning" wire:loading>POR FAVOR ESPERE</h6>
             <div class="card">
-                <div class="card-header">{{ __('Buscador') }}</div>
+                <div class="card-header">{{ __('Consultar url') }}</div>
 
                 <div class="card-body">
                     <div class="row">
                         <livewire:search />
-
-                        <livewire:results />
                     </div>
                 </div>
             </div>
@@ -21,3 +18,27 @@
     </div>
 </div>
 @endsection
+@push('js')
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        Livewire.on('buscar-ok', function () {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'success',
+                title: 'Busqueda realizada'
+            })
+        });
+    </script>
+@endpush
